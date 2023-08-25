0dff84f38b86afe987f1f970532368cd9fc07762
muaddib <muaddib@syneforge.com>
2023-08-25 16:08:03 +0300

    add routes

--- dist/js/app.js
+++ dist/js/app.js
@@ -369,16 +369,46 @@
             }));
         }
     }), 0);
-    const animateOnScrollElements = document.querySelectorAll(".animate-on-scroll");
-    function checkIfInView() {
-        animateOnScrollElements.forEach((element => {
-            const rect = element.getBoundingClientRect();
-            const windowHeight = window.innerHeight;
-            if (rect.top <= windowHeight && rect.bottom >= 0) element.classList.add("show");
+    function handleScroll() {
+        const headerTop = document.querySelector(".top-head");
+        const headerContainer = document.querySelector(".header__container");
+        if (window.location.pathname === "/" && window.innerWidth <= 768) if (window.scrollY >= 40) {
+            headerTop.style.transform = "translateY(-50%)";
+            headerContainer.style.height = "40px";
+            headerContainer.style.paddingBottom = "10px";
+            headerContainer.style.paddingTop = "10px";
+        } else {
+            headerTop.style.transform = "translateY(0)";
+            headerContainer.style.height = "auto";
+            headerContainer.style.paddingBottom = "none";
+            headerContainer.style.paddingTop = "none";
+        }
+    }
+    window.addEventListener("scroll", handleScroll);
+    document.querySelector(".top-head").style.transition = "transform 0.3s, height 0.3s";
+    function isElementInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+    }
+    function handleScrollAnimations() {
+        const animatedElements = document.querySelectorAll(".animate-on-scroll");
+        animatedElements.forEach((element => {
+            if (isElementInViewport(element)) element.classList.add("show"); else element.classList.remove("show");
         }));
     }
-    checkIfInView("");
-    window.addEventListener("scroll", checkIfInView);
+    window.addEventListener("scroll", handleScrollAnimations);
+    handleScrollAnimations();
+    function addMenuActive() {
+        const menuItems = document.querySelectorAll(".menu-head__item");
+        const currentURL = window.location.href;
+        const parts = currentURL.split("/");
+        const lastPart = parts[parts.length - 1].replace(".html", "");
+        menuItems.forEach((item => {
+            const dataMenuValue = item.getAttribute("data-menu");
+            if (lastPart === dataMenuValue) item.classList.add("ac-link");
+        }));
+    }
+    addMenuActive();
     window["FLS"] = true;
     isWebp();
     addLoadedClass();
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,23 +3,78 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
-const animateOnScrollElements = document.querySelectorAll(".animate-on-scroll");
+function handleScroll() {
+  const headerTop = document.querySelector(".top-head");
+  const headerContainer = document.querySelector(".header__container");
 
-function checkIfInView() {
-  animateOnScrollElements.forEach((element) => {
-    const rect = element.getBoundingClientRect();
-    const windowHeight = window.innerHeight;
+  // Перевіряємо чи це головна сторінка (ваш умовний шлях до головної сторінки)
+  if (window.location.pathname === "/" && window.innerWidth <= 768) {
+    // Перевіряємо поточне положення прокрутки
+    if (window.scrollY >= 40) {
+      // Якщо прокрутка більше або рівна 40px, ховаємо елементи з плавним ефектом
+      headerTop.style.transform = "translateY(-50%)";
+      headerContainer.style.height = "40px";
+      headerContainer.style.paddingBottom = "10px";
+      headerContainer.style.paddingTop = "10px";
+    } else {
+      // Якщо прокрутка менше 40px, показуємо елементи з плавним ефектом
+      headerTop.style.transform = "translateY(0)";
+      headerContainer.style.height = "auto";
+      headerContainer.style.paddingBottom = "none";
+      headerContainer.style.paddingTop = "none";
+    }
+  }
+}
 
-    if (rect.top <= windowHeight && rect.bottom >= 0) {
+// Додавання обробника подій "scroll" до вікна
+window.addEventListener("scroll", handleScroll);
+
+// Встановлення переходу (transition) для елементів для створення плавного ефекту
+document.querySelector(".top-head").style.transition =
+  "transform 0.3s, height 0.3s";
+
+// animate in viewport
+function isElementInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function handleScrollAnimations() {
+  const animatedElements = document.querySelectorAll(".animate-on-scroll");
+
+  animatedElements.forEach((element) => {
+    if (isElementInViewport(element)) {
       element.classList.add("show");
+    } else {
+      element.classList.remove("show");
     }
   });
 }
 
-// Initial check
-checkIfInView("");
+// Додавання обробника подій "scroll" до вікна
+window.addEventListener("scroll", handleScrollAnimations);
 
-// Check on scroll
-window.addEventListener("scroll", checkIfInView);
+// Виклик функції один раз для ініціалізації при завантаженні сторінки
+handleScrollAnimations();
 
-//-----------------------
+function addMenuActive() {
+  const menuItems = document.querySelectorAll(".menu-head__item");
+  const currentURL = window.location.href;
+  const parts = currentURL.split("/");
+  const lastPart = parts[parts.length - 1].replace(".html", "");
+
+  menuItems.forEach((item) => {
+    const dataMenuValue = item.getAttribute("data-menu");
+
+    if (lastPart === dataMenuValue) {
+      item.classList.add("ac-link");
+    }
+  });
+}
+addMenuActive();
